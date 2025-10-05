45f0e23bbc9acc2a28d76c22bd02f9e844b3f230
stormy/cli <chengcli@umich.edu>
2025-10-05 13:34:00 -0400

    Add saturn test

---
+++ src/paddle/__init__.py
@@ -0,0 +1,6 @@
+from .setup_profile import setup_profile
+from .write_profile import write_profile
+from .find_init_params import find_init_params
+
+__all__ = ["setup_profile", "write_profile", "find_init_params"]
+__version__ = "0.1.0"
