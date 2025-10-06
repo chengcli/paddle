4ef73748a845d1571e44666c20e53ffc9240c442
Cheng Li <69489965+chengcli@users.noreply.github.com>
2025-10-06 00:04:25 -0400

    Fix twine upload (#1)

--- src/paddle/__init__.py
+++ src/paddle/__init__.py
@@ -3,4 +3,4 @@ from .write_profile import write_profile
 from .find_init_params import find_init_params
 
 __all__ = ["setup_profile", "write_profile", "find_init_params"]
-__version__ = "0.1.0"
+__version__ = "1.1.0"
